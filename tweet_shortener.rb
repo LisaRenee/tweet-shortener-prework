6e50ed8c7ad7b15830b0911b4b8699d81696cdcc
Lisa Scott <lrscot@aol.com>
2019-02-13 07:59:56 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -36,11 +36,12 @@ def bulk_tweet_shortener(tweets)
 end
 
 def selective_tweet_shortener(tweet)
-    if tweet.length < 146
+    if tweet.length < 140
       puts tweet
       return tweet
     else
       puts word_substituter(tweet)
+      return word_substituter(tweet)
     end
 tweet
 end
